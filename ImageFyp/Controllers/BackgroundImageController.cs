84cf74c297a6a1ff89d9520e0a5e3de241a603e2
DESKTOP-FBE7166\Blal <blalkhan94@gmail.com>
2021-12-19 19:18:41 +0000

    Add project files.

---
+++ ImageFyp/Controllers/BackgroundImageController.cs
@@ -0,0 +1,52 @@
+﻿using ImageFyp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ImageFyp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BackgroundImageController : ControllerBase
+    {
+        // GET: api/<BackgroundImageController>
+        [HttpGet]
+        public IEnumerable<BackgroundImage> Get()
+        {
+            yield return new BackgroundImage
+            {
+                Id = 1,
+                Name = "yao.jpg"
+            };
+        }
+
+        // GET api/<BackgroundImageController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<BackgroundImageController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<BackgroundImageController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<BackgroundImageController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ImageFyp/Models/BackgroundImage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImageFyp.Models
+{
+    public class BackgroundImage
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+    }
+}
