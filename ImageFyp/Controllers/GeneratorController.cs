98ee94a9baa303b7311dc21f7b1cc3e0a50f2256
Blal <blalkhan94@gmail.com>
2022-01-07 19:40:47 +0000

    Images can be omitted with text.

--- ImageFyp/Controllers/GeneratorController.cs
+++ ImageFyp/Controllers/GeneratorController.cs
@@ -1,6 +1,8 @@
 ﻿using ImageFyp.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Drawing;
+using System.IO;
 
 namespace ImageFyp.Controllers
 {
@@ -9,20 +11,51 @@ namespace ImageFyp.Controllers
     public class GeneratorController : Controller
     {
         // GET: Generator
-
         [HttpGet("{id}/{userText}")]
         public ActionResult GetGenerator(int id, string userText)
         {
 
-           
+
             var selectedBackgroundImage = BackgroundData.GetBackgroundImageById(id);
             var imageUrl = selectedBackgroundImage.Url;
-            var imageName = selectedBackgroundImage.Name;
-            byte[] b = System.IO.File.ReadAllBytes(imageUrl);
-            return File(b, imageName + "/jpeg");
+
+            PointF firstLocation = new PointF(10f, 10f);
+
+            Bitmap bitmap = (Bitmap)Image.FromFile(imageUrl);
+
+            using (Graphics graphics = Graphics.FromImage(bitmap))
+            {
+                using (Font arialFont = new Font("Arial", 100))
+                {
+                    graphics.DrawString(userText, arialFont, Brushes.Blue, firstLocation);
+                }
+
+            }
+
+            using (var stream = new MemoryStream())
+            {
+                bitmap.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+                return File(stream.ToArray(), "image/jpeg");
+            }
+
+            
+
         }
 
-         // POST api/Generator
+        
+        //[HttpGet("{id}/{userText}")]
+        //public ActionResult GetGenerator(int id, string userText)
+        //{
+
+
+        //    var selectedBackgroundImage = BackgroundData.GetBackgroundImageById(id);
+        //    var imageUrl = selectedBackgroundImage.Url;
+        //    var imageName = selectedBackgroundImage.Name;
+        //    byte[] b = System.IO.File.ReadAllBytes(imageUrl);
+        //    return File(b, imageName + "/jpeg");
+        //}
+
+        // POST api/Generator
         [HttpPost]
         public ActionResult Post([FromBody] GeneratorInstructions instructions)
         {
@@ -34,6 +67,7 @@ namespace ImageFyp.Controllers
             byte[] b = System.IO.File.ReadAllBytes(imageUrl);
             return File(b, "image/jpeg");
         }
+
 
         
     }
